3dc13774c9c1b03a217a7a8d60340e05bf340d9f
Wahib Kapdi <kanchuking1@gmail.com>
2023-12-09 17:57:24 +0530

    Image Generation Logic Added

--- frontend/src/components/StableDiffusionHomePage/StableDiffusionHomePage.tsx
+++ frontend/src/components/StableDiffusionHomePage/StableDiffusionHomePage.tsx
@@ -1,12 +1,48 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Container, Box, Toolbar, TextField, IconButton, Paper, Typography } from '@mui/material';
+import { Container, Box, Toolbar, TextField, IconButton, Paper, Typography, LinearProgress } from '@mui/material';
 import { Search, AutoAwesome, AutoFixHigh } from '@mui/icons-material';
 
 export const StableDiffusionHomePage = () => {
-  const [prompt, setPrompt] = useState<string | null>();
+  const [prompt, setPrompt] = useState<string>();
+  const [isGenerating, setIsGenerating] = useState<boolean>(false);
+  const [hasGenerated, setHasGenerated] = useState<boolean>(false);
+  
   const handleInput = (event: any) => {
     setPrompt(event.target.value);
+  }
+
+  const SubmitButton = () => {
+    return <IconButton 
+      edge="end" color="primary" 
+      disabled={prompt?.length !== undefined && prompt?.length < 5 || prompt?.length === undefined || isGenerating}
+      onClickCapture={() => {
+        setIsGenerating(true);
+        setTimeout(() => {
+          setIsGenerating(false);
+          setHasGenerated(true);
+        }, 3000);
+      }}>
+      <AutoFixHigh />
+    </IconButton>
+  }
+
+  const RenderInputBar = () => {
+    return <TextField
+      id="prompt-input"
+      value={prompt}
+      variant='outlined'
+      onChange={handleInput}
+      sx={{
+        width: 600
+      }}
+      label='What Image do you want to generate?'
+      placeholder='Minimum 5 characters'
+      InputProps={{
+        endAdornment: <SubmitButton />
+      }}
+      disabled={isGenerating}>
+    </TextField>;
   }
 
   return (
@@ -42,21 +78,22 @@ export const StableDiffusionHomePage = () => {
                 Generate your own NFTs
               </Typography>
             </Box>
-            <TextField
-              id="prompt-input"
-              value={prompt}
-              variant='outlined'
-              onInputCapture={handleInput}
-              sx={{
-                width: 600
-              }}
-              placeholder='What Image do you want to generate?'
-              InputProps={{
-                endAdornment: <IconButton edge="end" color="primary">
-                <AutoFixHigh />
-              </IconButton>
-              }}>
-            </TextField>
+            {RenderInputBar()}
+            <Box sx={{
+              my: 2,
+              mx: 1
+            }}>
+              {isGenerating && <LinearProgress sx={{
+                borderRadius: '2px'
+              }} />}
+              {hasGenerated && <Box sx={{alignItems: 'center', justifyContent: 'center', display: 'flex'}}>
+                  <Paper elevation={4} sx={{
+                  p: 1
+                }}>
+                  <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e' height={500} width={450}></img>
+                </Paper>
+                </Box>}
+            </Box>
           </Paper>
         </Container>
     </Box>
